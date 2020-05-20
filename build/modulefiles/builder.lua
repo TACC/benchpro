d6894845fa5ccd72937aaf47f319329438ff8445
mcawood <mcawood@tacc.utexas.edu>
2020-05-19 22:07:58 -0500

    removed builder.lua from .ignore

---
+++ build/modulefiles/builder.lua
@@ -0,0 +1,14 @@
+
+local fn             = myFileName()                      
+local full           = myModuleFullName()                
+local loc            = fn:find("build",1,true)-2        
+local project_dir    = fn:sub(1,loc)                    
+
+
+setenv("TACC_BUILDER_DIR", project_dir)
+prepend_path("PATH",       project_dir)
+prepend_path( "MODULEPATH" , pathJoin( project_dir , "build/modulefiles" ))
+
+always_load("python3")
+
+family("builder")
