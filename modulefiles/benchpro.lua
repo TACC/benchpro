6589411ccc8048eee6664e702114f5dda8647b12
mcawood <mcawood@tacc.utexas.edu>
2022-07-29 12:51:50 -0500

    revised benchpro modulefile

--- modulefiles/benchpro.lua
+++ modulefiles/benchpro.lua
@@ -2,22 +2,25 @@
 
 
 
+setenv("BP_NOTICE", "1")
+
+
 -- Resolve user environment variables, $HOME, $SCRATCH, etc
-local bp_home         = ""
+local bp_home         = 
 local i,j, envName, tail = bp_home:find("$([a-zA-Z0-9]+)/(.*)")
 if (i) then
   bp_home = pathJoin(os.getenv(envName), tail)
 end
 setenv("BP_HOME", bp_home)
 
-local bp_apps         = ""
+local bp_apps         = 
 local i,j, envName, tail = bp_apps:find("$([a-zA-Z0-9]+)/(.*)")
 if (i) then
   bp_apps = pathJoin(os.getenv(envName), tail)
 end
 setenv("BP_APPS",     bp_apps)
 
-local bp_results      = ""
+local bp_results      = 
 local i,j, envName, tail = bp_results:find("$([a-zA-Z0-9]+)/(.*)")
 if (i) then
   bp_results = pathJoin(os.getenv(envName), tail)
@@ -33,18 +36,12 @@ set_alias("cdb", "cd $BP_HOME")
 -- add user's application directory tree to MODULEPATH
 prepend_path("MODULEPATH" ,  pathJoin(bp_apps, "modulefiles" ))
 
--- Prompt user to clone user files if not present
-if ( mode() == "load" and not isDir(bp_home) ) then
-  LmodMessage(bp_home .. " not found")
-  LmodMessage("Get BenchPRO user files with:")
-  LmodMessage("git clone https://github.com/TACC/benchpro.git $HOME/benchpro")
-  LmodMessage("")
-end
-
 always_load("python3")
 family("benchpro")
 
 -- Add BenchPRO module path to PYTHONPATH
-prepend_path("PYTHONPATH",         pathJoin(bp_site, "python/lib/python" .. py_version, "site-packages/" ))
-prepend_path("PATH",               pathJoin(bp_site, "python/bin" ))
+local py_version =
+local bps_site =
+prepend_path("PYTHONPATH",         pathJoin(bps_site, "python/lib/python" .. py_version, "site-packages/" ))
+prepend_path("PATH",               pathJoin(bps_site, "python/bin" ))
 
