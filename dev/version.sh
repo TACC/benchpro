2f8b6d4d12a08c7bbb34995c29314b678b0bd0fb
mcawood <mcawood@tacc.utexas.edu>
2022-12-07 15:59:49 -0600

    minor syntax

--- dev/version.sh
+++ dev/version.sh
@@ -16,12 +16,7 @@ echo "benchpro v${1}" > $HOME/benchpro/.version
 date +'%Y-%m-%d %H:%m:%S' >> $HOME/benchpro/.version
 
 # Update benchpro version check
-sed -i "/        required_version =/c\        required_version = \"${1}\"" ./src/benchpro
+sed -i "/    required_version =/c\    required_version = \"${1}\"" ./src/benchpro
 
-echo "Push user repo before reinstalling"
-echo "--------------"
-echo "git -C \$HOME/benchpro add ."
-echo "git -C \$HOME/benchpro commit -m 'updated version info'"
-echo "git -C \$HOME/benchpro push "
 echo
 echo "Version updated"
