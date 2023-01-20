9ab4ee55c8ca377c852bacef666aea0bdb16d027
mcawood <mcawood@tacc.utexas.edu>
2023-01-20 14:27:39 -0600

    added bp_repo context

--- modulefiles/benchpro.lua
+++ modulefiles/benchpro.lua
@@ -11,6 +11,13 @@ if (i) then
 end
 setenv("BP_HOME", bp_home)
 
+local bp_repo         =
+local i,j, envName, tail = bp_repo:find("$([a-zA-Z0-9]+)/(.*)")
+if (i) then
+  bp_repo = pathJoin(os.getenv(envName), tail)
+end
+setenv("BP_REPO", bp_repo)
+
 local bp_apps         = 
 local i,j, envName, tail = bp_apps:find("$([a-zA-Z0-9]+)/(.*)")
 if (i) then
@@ -29,7 +36,7 @@ setenv("BP_RESULTS",  bp_results)
 set_alias("bp", "benchpro")
 set_alias("bps", "benchset")
 set_alias("cdb", "cd $BP_HOME")
-set_alias("toggle-bps", "source $BPS_BIN/toggle_dev_prod")
+set_alias("bp_switch", "source $BPS_BIN/toggle_dev_prod")
 
 -- add user's application directory tree to MODULEPATH
 prepend_path("MODULEPATH" ,  pathJoin(bp_apps, "modulefiles" ))
