b624bfc218b92bad80789bee02566df24246bb5c
mcawood <mcawood@tacc.utexas.edu>
2023-06-20 14:10:18 -0500

    1.8.0

--- dev/version.sh
+++ dev/version.sh
@@ -11,12 +11,5 @@ sed -i "/version=/c\    version=\'${1}\'," ./setup.py
 # Update site.sh
 sed -i "/set BPS_VERSION /c\set BPS_VERSION \"${1}\"" ./site.sh
 
-# Update version in user repo
-echo "benchpro v${1}" > $HOME/benchpro/.version
-date +'%Y-%m-%d %H:%m:%S' >> $HOME/benchpro/.version
-
-# Update benchpro version check
-sed -i "/    required_version =/c\    required_version = \"${1}\"" ./src/benchpro
-
 echo
 echo "Version updated"
