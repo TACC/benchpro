6cecd05a8db975240b2ce209570a8ca2c65a8a23
mcawood <mcawood@tacc.utexas.edu>
2023-10-24 11:09:10 -0500

    version update

--- site.sh
+++ site.sh
@@ -9,7 +9,7 @@ set() {
 
 # SETUP
 set BPS_SYSTEM      $TACC_SYSTEM
-set BPS_VERSION "1.8.2"
+set BPS_VERSION "1.8.3"
 [[ -z $BP_DEV ]] && set BP_DEV 1
 set BUILD_HASH `echo $RANDOM | md5sum | head -c 8`
 
